2528b20127ef182177924543b2d02726ec09f738
Turi020 <turancan.dur@student.htldornbirn.at>
2025-10-15 09:02:28 +0200

    TS2 Datentypen fertig

---
+++ Typescript/Task2/datentypen.ts
@@ -0,0 +1,11 @@
+let a:number=4;
+let title:string = "hansi";
+let isMale:boolean=false;
+
+function sayHello(title: string): void {
+
+    console.log(`My name is ${title}`);
+
+
+}
+sayHello("hans");
